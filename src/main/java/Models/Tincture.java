c9a0569039c8c8549e6651f4443f3ec3767b2e33
hillary <hillary@new-host.home>
2020-11-09 20:16:23 -0500

    tinctureproperties

--- src/main/java/Models/Tincture.java
+++ src/main/java/Models/Tincture.java
@@ -1,4 +1,10 @@
 package Models;
 
 public class Tincture {
+    private int id;
+    private String name;
+    private String brand;
+    private int qty;
+    private String ingred;
+    private float price;
 }
