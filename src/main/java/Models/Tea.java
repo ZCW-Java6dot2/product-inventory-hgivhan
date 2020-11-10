694b8269850b4d4afcf5318b92852deb1615c2cd
hillary <hillary@new-host.home>
2020-11-09 20:08:55 -0500

    classFields

--- src/main/java/Models/Tea.java
+++ src/main/java/Models/Tea.java
@@ -1,4 +1,10 @@
 package Models;
 
 public class Tea {
+    private int id;
+    private String name;
+    private String brand;
+    private int qty;
+    private String ingred;
+    private float price;
 }
